cb5dc53f115496a089b6b902752daa3f51979954
amarkiewicz <adam.markiewicz@scalosoft.com>
2022-09-19 15:51:13 +0200

    Added Authentication

--- main.py
+++ main.py
@@ -2,7 +2,19 @@ from _config import *
 from flask import Flask, render_template, jsonify, request
 import psycopg2
 
+
+###### Define Admin User List #######
+
 adminUserList = ('bartek.wruszczak@scalosoft.com')
+
+usrname = request.headers.get('X-MS-CLIENT-PRINCIPAL-NAME')
+if usrname in adminUserList:
+    usrlvl = 'admin'
+else:
+    usrlvl = 'read only'
+
+
+###### Define DB Connection #######
 
 try:
     conn = psycopg2.connect(host=db_host, port=db_port, user=db_user, password=db_pass, database=db_dbname)
@@ -11,54 +23,15 @@ except:
     exit()
 
 
-###### Function Definition ######
-
-def updateVacancy(updtype, vacid):
-    add = 50
-    qry = ''
-    if (updtype == 'up' or updtype == 'down'):
-        if updtype == 'down':
-            add = add * (-1)
-        if vacid.isnumeric():
-            qry = 'INSERT INTO mi.t_vacancy_additional_rank(vacancy_id, additional_rank, mod_date) VALUES (%s, %s, NOW());' % (vacid, add)
-            mycursor.execute(qry)
-            conn.commit()
-
-    return jsonify({'Vacancy ID': vacid,
-                    'Update Type': updtype,
-                    'Update Value': add,
-                    'Executed Query': qry})
-
-
-def updateClient(ccid, fieldname, fieldvalue):
-    qry = ''
-    if ccid.isnumeric():
-        if fieldname == 'client_type' or fieldname == 'client_probability' or fieldname == 'client_recruitment' or fieldname == 'client_relations':
-            qry = 'INSERT INTO mi.t_client_attributes (client_corporation_id, %s, mod_date) VALUES (%s, \'%s\', NOW()) ON CONFLICT (client_corporation_id) DO UPDATE SET %s = \'%s\', mod_date = NOW();' % (fieldname, ccid, fieldvalue, fieldname, fieldvalue)
-            mycursor.execute(qry)
-            conn.commit()
-
-    return jsonify({'Client Corp ID': ccid,
-                    'FieldName': fieldname,
-                    'FieldValue': fieldvalue,
-                    'Executed Query': qry})
-
-
-###### Flask App Definition #######
+###### Define Flask App #######
 
 app = Flask(__name__, static_folder='templates/css')
 
-### Flask App Pages ###
 
+### Define App Pages ###
 
 @app.route('/')
-def mainpage():
-    usrname = request.headers.get('X-MS-CLIENT-PRINCIPAL-NAME')
-    if usrname in adminUserList:
-        usrlvl = 'Admin'
-    else:
-        usrlvl ='ReadOnly'
-
+def mainpage(usrname, usrlvl):
     mycursor.execute("SELECT * FROM mi.v_vacancy_priority_v2")
     data = mycursor.fetchall()
     return render_template('index.html', data=data, usrlvl=usrlvl, usrname=usrname)
@@ -70,19 +43,6 @@ def clientatr():
     data = mycursor.fetchall()
     return render_template('clientatr.html', data=data)
 
-
-### Flask Api ###
-
-@app.route('/api/<updtype>/<vacid>')
-def updateVacancyApi(updtype, vacid):
-    resp = updateVacancy(updtype, vacid)
-    return resp
-
-
-@app.route('/api/<ccid>/<fieldname>/<fieldvalue>')
-def updateClientApi(ccid, fieldname, fieldvalue):
-    resp = updateClient(ccid, fieldname, fieldvalue)
-    return resp
 
 
 ### Run App ###
